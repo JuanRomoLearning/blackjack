bc1d96e704d88c64d728c02ed5605f6e047d5bcc
Juan Romo <juanromolearning@gmail.com>
2021-06-28 12:12:26 -0500

    Add deck creation function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function generateSixDeck() {
+    const suits = ["Heart", "Spade", "Diamond", "Club"];
+    const values = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "King", "Queen", "Jack"];
+
+    const sixDeck = [];
+    for (let deck = 0; deck < 6; deck++) {
+        for (let suit = 0; suit < 4; suit++) {
+            for (let value = 0; value < 13; value++) {
+                sixDeck.push({
+                    suit: suits[suit],
+                    value: values[value]
+                });
+            }
+        }
+    }
+    return sixDeck;
+}
+
 console.log("Blackjack")
+
+let deck = generateSixDeck();
+console.log(deck);
