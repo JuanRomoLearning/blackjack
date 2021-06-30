d5cb919d3a494b06a6d076a0dd64e7b0bcd8262a
Juan Romo <juanromolearning@gmail.com>
2021-06-30 18:36:38 -0500

    Add card dealing function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function generateSixDeck() {
     const values = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "King", "Queen", "Jack"];
 
     const sixDeck = [];
-    for (let deck = 0; deck < 6; deck++) {
+    for (let deck = 0; deck < DECKS; deck++) {
         for (let suit = 0; suit < 4; suit++) {
             for (let value = 0; value < 13; value++) {
                 sixDeck.push({
@@ -25,8 +25,23 @@ function shuffleDeck(deck) {
     }
 }
 
-console.log("♠ ♥ Blackjack ♣ ♦");
+function dealCards(deck) {
+    if ((DECKS * 52) * DECK_PENETRATION > deck.length) {
+        deck.length = 0;
+        deck.push(...generateSixDeck());
+        shuffleDeck(deck);
+    }
 
-let deck = generateSixDeck();
+    const cardsDealt = [deck.pop(), deck.pop(), deck.pop(), deck.pop()];
+    return [[cardsDealt[0], cardsDealt[2]], [cardsDealt[1], cardsDealt[3]]];
+}
+
+// Game Settings
+const DECKS = 6;
+const DECK_PENETRATION = .75; // Errors can occur if the deck penetration is too high and cards begin to be dealt from an empty deck
+
+const deck = generateSixDeck();
 
 shuffleDeck(deck);
+
+let [userHand, dealerHand] = dealCards(deck);
