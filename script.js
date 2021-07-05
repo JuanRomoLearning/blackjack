fbd06dd71584119d11f90aa416ce5c9cb7a02ad1
Juan Romo <juanromolearning@gmail.com>
2021-07-05 16:49:46 -0500

    Add hand score calculating function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generateDeck() {
     const suits = ["Club", "Diamond", "Heart", "Spade"];
-    const values = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"];
+    const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
     const sixDeck = [];
     for (let deck = 0; deck < DECKS; deck++) {
@@ -25,10 +25,12 @@ function shuffleDeck(deck) {
     }
 }
 
+// Hard Coded to only support 1 player alongside the dealer
 function dealCards(deck) {
+    // Create a new shuffled deck if we reach the deck penetration limit
     if ((DECKS * 52) * DECK_PENETRATION > deck.length) {
         deck.length = 0;
-        deck.push(...generateSixDeck());
+        deck.push(...generateDeck());
         shuffleDeck(deck);
     }
 
@@ -36,12 +38,38 @@ function dealCards(deck) {
     return [[cardsDealt[0], cardsDealt[2]], [cardsDealt[1], cardsDealt[3]]];
 }
 
+function calculateHandScore(hand) {
+    hand.sort((firstCard) => firstCard.value === "Ace" ? 1 : -1); // Move aces to the back to determine their value after all other cards are accounted for
+
+    let handScore = 0;
+    for (let card of hand) {
+        switch (card.value) {
+            case "Ace":
+                handScore += handScore + 11 <= 21 ? 11 : 1;
+                break;
+            case "Jack":
+            case "Queen":
+            case "King":
+                handScore += 10;
+                break;
+            default:
+                handScore += parseInt(card.value);
+                break;
+        }
+    }
+    return handScore;
+}
+
 // Game Settings
 const DECKS = 6;
-const DECK_PENETRATION = .75; // Errors can occur if the deck penetration is too high and cards begin to be dealt from an empty deck
+const DECK_PENETRATION = .75; // Errors can occur if the deck penetration is set too high and cards begin to be dealt from an empty deck
 
-const deck = generateSixDeck();
+const deck = generateDeck();
 
 shuffleDeck(deck);
 
 let [userHand, dealerHand] = dealCards(deck);
+
+console.log(`Dealer's Hand: ${dealerHand[0].value} of ${dealerHand[0].suit}s for a hand score of ${calculateHandScore(dealerHand.slice(0, 1))}`);
+
+console.log(`Your Hand: ${userHand[0].value} of ${userHand[0].suit}s and ${userHand[1].value} of ${userHand[1].suit}s for a hand score of ${calculateHandScore(userHand)}`);
